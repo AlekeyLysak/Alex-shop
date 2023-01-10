e28c7eb104374468542da89f0b12b6dd75f10fa1
Alex Lysak <117944184+AlekeyLysak@users.noreply.github.com>
2023-01-10 17:45:13 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+const count = document.querySelector(".red");
+const backColCount = document.querySelector(".products-count");
+const btns = document.querySelectorAll(".add-btn");
+const arrButtons = Array.from(btns);
+const addToCart = () => {
+    count.textContent++;
+    if (count.textContent !== 0) { backColCount.classList.add(".red") };
+};
+arrButtons.forEach(x => x.addEventListener("click", addToCart));
