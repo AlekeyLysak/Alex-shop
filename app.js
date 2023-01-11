1e36b26c6b96236c23f1e126be1bea64e19fc4eb
Alex Lysak <117944184+AlekeyLysak@users.noreply.github.com>
2023-01-11 20:20:04 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
-const count = document.querySelector(".red");
-const backColCount = document.querySelector("#products-count");
-const btns = document.querySelectorAll(".add-btn");
-const arrButtons = Array.from(btns);
-const addToCart = () => {
-    count.textContent++;
-    if (count.textContent !== 0) { backColCount.classList.add(".red") };
-};
-arrButtons.forEach(x => x.addEventListener("click", addToCart));
+let productsCountEL = document.getElementById("products-count");
+
+ let addToCartBtns = document.querySelectorAll(".add-btn");
+ console.log(addToCartBtns);
+
+ for (let i = 0; i < addToCartBtns.length; i++){
+     addToCartBtns[i].addEventListener("click", function () {
+         productsCountEL.textContent = +productsCountEL.textContent + 1;
+        
+     });
+}
+
+
+// LIKE
+ 
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+likeBtns.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+        // if (btn.classList.contains("liked")) {
+        //     btn.classList.remove("liked");
+        // } else {
+        //     btn.classList.add("liked");
+        // }
+        e.target.classList.toggle("liked");
+        e.target.style.transition = "all 0.4s ease-out";
+    })
+);
+
+let moreDetailsBtns = document.querySelectorAll(".more-detail");
+let modal = document.querySelector(".modal");
+
+let closeBtn = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+}
+
+function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+}
+modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+
+// slider
+
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots:true,
+});
